41307caf15ca7655853e803255685ff4b7d6841d
Alan <alan-deivids@hotmail.com>
2026-08-01 18:46:29 -0300

    chore: remove pasta app antiga da raiz

--- app/api/v1/api.py
+++
@@ -1,7 +0,0 @@
-from fastapi import APIRouter
-from app.api.v1.endpoints import auth, clientes, funcionarios
-
-api_router = APIRouter()
-api_router.include_router(auth.router, prefix="/auth", tags=["Autenticação"])
-api_router.include_router(clientes.router, prefix="/clientes", tags=["Clientes"])
-api_router.include_router(funcionarios.router, prefix="/funcionarios", tags=["Funcionários"])
