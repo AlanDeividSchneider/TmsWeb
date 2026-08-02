b7d7b4bddcf0d7876925751560c2ff791d8db41d
Alan <alan-deivids@hotmail.com>
2026-08-02 12:06:00 -0300

    Criando arquivo de exemplo do .env, alterando frontend e backend para que dependam totalmente do .env, removendo todas as informações que estavam diretamente no código

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -1,14 +1,19 @@
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
-    PROJECT_NAME: str = "Sistema TMS API"
+    PROJECT_NAME: str = "TMS Web"
     API_V1_STR: str = "/api/v1"
+    
     SECRET_KEY: str
     ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 480
+    
     DATABASE_URL: str
 
-    class Config:
-        env_file = ".env"
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        env_file_encoding="utf-8",
+        extra="ignore"
+    )
 
 settings = Settings()
